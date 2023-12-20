586520b8a8dbd875c6793ef8633d99560230162f
Omer Gindi <110094002+RPN17M2G@users.noreply.github.com>
2023-12-20 15:42:22 +0200

    Update Link_Convertor_md_to_docx_pandoc_plugin.lua
    Added support for wikilinks that follow the format: [[target|text_to_show]] or [[target_and_text_to_show]]

--- Link_Convertor_md_to_docx_pandoc_plugin.lua
+++ Link_Convertor_md_to_docx_pandoc_plugin.lua
@@ -1,8 +1,7 @@
 function Link(el)
-
   local stripped_target = el.target:gsub("%%20", " ") -- Format spaces
   local does_have_md = false
-  local does_have_hashtag, stripped_target = cutFirstPartBySep(stripped_target, "#") -- Remove file name for header links between files
+  local does_have_hashtag, stripped_target = cutPartBySep(stripped_target, "#", false) -- Remove file name for header links between files
   local target_id
   local hyperlink
 
@@ -22,10 +21,7 @@ function Link(el)
     stripped_target = "#"..stripped_target
   end
 
-  target_id = stripped_target:gsub(" & ", " ") -- Special case of which & is wrapped with spaces
-  target_id = target_id:gsub("[%(%)%[%]&]", "") -- Docx bookmarks do not work with & or with paranthesis
-  target_id = target_id:gsub(" ", "-"):lower() --The id of heading is in the format of: "word-word" in lowercase
-  --That way I don't link the link to itself of any other element that has the content of the header
+  target_id = parseTargetId(stripped_target)
 
   hyperlink = pandoc.Link(stripped_target:gsub("#", ""), target_id)
 
@@ -41,16 +37,28 @@ function Image(img)
   return img
 end
 
+function parseTargetId(text)
+  -- Parse target from text to id in the format of heading.
+  -- The id of heading is in the format of: "word-word" in lowercase
+  -- That way I don't link the link to itself of any other element that has the content of the header
 
-function cutFirstPartBySep (inputstr, seperator)
-  -- Cut the first part of the string by seperator and check if the string have the seperator
+  target_id = text:gsub(" & ", " ") -- Special case of which & is wrapped with spaces
+  target_id = target_id:gsub("[%(%)%[%]&]", "") -- Docx bookmarks do not work with & or with paranthesis
+  target_id = target_id:gsub(" ", "-"):lower() -- The id of heading is in the format of: "word-word" in lowercase
+  --That way I don't link the link to itself of any other element that has the content of the header
+
+  return target_id
+end
+
+function cutPartBySep (inputstr, seperator, firstPart)
+  -- Cut the first or the second part of the string by seperator and check if the string have the seperator
   if seperator == nil then
     seperator = "%s"
   end
-  local result = "#"
+  local result = seperator
   local i = 0
   for str in string.gmatch(inputstr, "([^"..seperator.."]+)") do
-    if i > 0 then      
+    if (i > 0 and firstPart == false) or (i == 0 and firstPart) then      
       result = result..str
     end
     i = i + 1
@@ -61,3 +69,54 @@ function cutFirstPartBySep (inputstr, seperator)
   return i > 1 or inputstr:sub(1, #seperator) == seperator, result -- Return the result and if the string has hashtag
 end
 
+local link = ""
+local isInLink = false
+
+function Str (el)
+  -- Support for wikilinks
+  -- Can support wikilinks that are in the format of: [[target|text_to_show]] or [[target_and_text_to_show]]
+
+  local elStr = tostring(el):gsub('"', ""):gsub("Str ", "") -- Convert string to a string type
+  local firstPart
+  local secondPart
+  local doesHave
+
+  if string.find(elStr ,"%[%[") ~= nil and string.find(elStr ,"%]%]") ~= nil then -- 1 word wikilink
+    link =  elStr:gsub("%[%[", ""):gsub("%]%]", "") -- Mark as link
+
+    -- Cut the wikilink to the target and the content(If there is only target it will return the original text
+    -- which means that the content and the target will be the same)
+    doesHave, secondPart = cutPartBySep(link, '|', false)
+    doesHave, firstPart = cutPartBySep(link, '|', true)
+    doesHave, firstPart = cutPartBySep(firstPart, "#", false) -- Remove file name
+    firstPart = firstPart:gsub("|", ""):gsub("%%20", " "):gsub("#", "")
+    secondPart = secondPart:gsub("|", "")
+
+    return pandoc.Link(secondPart, "#"..parseTargetId(firstPart)) -- Convert target to target_id
+
+  elseif string.find(elStr ,"%[%[") ~= nil then -- Starting a link that is longer than 1 word
+    isInLink = true -- Mark start of link
+    link = elStr:gsub("%[%[", "") -- Mark as link
+
+  elseif string.find(elStr ,"%]%]") ~= nil then -- End of link
+    link = link .. " " .. elStr:gsub("%]%]", "")
+    isInLink = false
+
+    -- Cut the wikilink to the target and the content(If there is only target it will return the original text
+    -- which means that the content and the target will be the same)
+    doesHave, secondPart = cutPartBySep(link, '|', false)
+    doesHave, firstPart = cutPartBySep(link, '|', true)
+    doesHave, firstPart = cutPartBySep(firstPart, "#", false) -- Remove file name
+    firstPart = firstPart:gsub("|", ""):gsub("%%20", " ")
+    secondPart = secondPart:gsub("|", "")
+
+    return pandoc.Link(secondPart, "#"..parseTargetId(firstPart)) -- Convert target to target_id
+
+  elseif isInLink then -- Middle of a link
+    link = link .. " " .. elStr
+  end
+
+  if isInLink then
+    return "" -- Delete link's text
+  end
+end
